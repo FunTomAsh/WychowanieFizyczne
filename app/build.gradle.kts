2ff23cc92150f69b705fefdc03967b1e18ffc701
Tomasz Karpiej <tomash342@gmail.com>
2024-04-18 18:36:00 +0200

    Menu główne

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(libs.glide)
-    annotationProcessor(libs.glideCompiler)
 }
--- app/src/main/java/com/example/wychowaniefizyczne/MainActivity.java
+++ app/src/main/java/com/example/wychowaniefizyczne/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.wychowaniefizyczne;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.appcompat.app.AppCompatActivity;
-import com.bumptech.glide.Glide;
+
+//import com.dudegym.dudefitnessworkout.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,12 +22,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    private void showTask() {
-        ImageView imageView = findViewById(R.id.image_exercise);
-        Glide.with(this)
-                .load("https://previews.123rf.com/images/captainvector/captainvector1602/captainvector160212561/52717036-dziewczyna-uprawiania-jogi-w-sk%C5%82on-tu%C5%82owia-stanowi%C4%85.jpg")
-                .into(imageView);
     }
 }
